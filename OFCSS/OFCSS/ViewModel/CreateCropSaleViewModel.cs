1b3b6b4487b4dcd8061ff725594380dba7099c58
Gautam Nayak <88781537+gautamnayak522@users.noreply.github.com>
2021-09-12 02:10:20 +0530

    init

---
+++ OFCSS/OFCSS/ViewModel/CreateCropSaleViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OFCSS.ViewModel
+{
+    public class CreateCropSaleViewModel
+    {
+        [Key]
+        public int c_id { get; set; }
+
+        [Required]
+        public string cname { get; set; }
+
+        [Required]
+        public string cquantity { get; set; }
+
+        [Required]
+        public string cprice { get; set; }
+
+        [Required]
+        public string cdiscription { get; set; }
+    }
+}
