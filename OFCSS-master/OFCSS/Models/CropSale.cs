ad47621254982e1f04334e5d0b227d878e467d12
Gautam Nayak <88781537+gautamnayak522@users.noreply.github.com>
2021-09-12 02:07:40 +0530

    init

---
+++ OFCSS-master/OFCSS/Models/CropSale.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OFCSS.Models
+{
+    public class CropSale
+    { 
+
+        [Key]
+        public int c_id { get; set; }
+
+        [Required]
+        public string cname { get; set; }
+
+        [Required]
+        public string cquantity { get; set; }
+
+        [Required]
+        public string cprice { get; set; }
+
+        [Required]
+        public string cdiscription { get; set; }
+
+        public int f_id { get; set; }
+        public Farmer Farmer { get; set; }
+    }
+}
